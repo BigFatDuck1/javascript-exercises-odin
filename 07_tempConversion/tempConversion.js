6ddc23ee6a95d4152e9fc3684b2ee5ad13868ba7
Clement <hoc1@connect.hku.hk>
2023-09-03 01:54:04 +0800

    07_tempConversion completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+
+  let convertedTemp = 0;
+
+  convertedTemp = (temp - 32) * 5/9;
+  convertedTemp = convertedTemp.toFixed(1);
+
+  return parseFloat(convertedTemp);
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+
+  let convertedTemp = 0;
+
+  convertedTemp = (temp*9/5) + 32;
+  convertedTemp = convertedTemp.toFixed(1);
+
+  return parseFloat(convertedTemp);
+
 };
 
 // Do not edit below this line
