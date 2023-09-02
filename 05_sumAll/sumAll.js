0ed1150d6a434bab8e2bed7fb9d43288df458c0d
Clement <hoc1@connect.hku.hk>
2023-09-02 22:25:57 +0800

    05_sumAll completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,51 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+
+    //Initial checks
+    if (int1 < 0 || int2 < 0) {
+        return "ERROR"
+    }
+    if (typeof(int1) != "number" || typeof(int2) != "number") {
+        return "ERROR"
+    }
+
+    //Define a sum function
+    const sumArray = (arrayGiven) => {
+        let resultOfSum = 0;
+        for (i = 0; i < arrayGiven.length; i++) {
+            resultOfSum += arrayGiven[i];
+        }
+
+        return resultOfSum;
+    };
+
+    //Fill an array with all the intergers from smallerInt to biggerInt
+    //Assign which one is smaller which one is bigger
+    let biggerInt = 0;
+    let smallerInt = 0;
+    if (int1 > int2) {
+        biggerInt = int1;
+        smallerInt = int2;
+    }
+    else if (int2 > int1) {
+        biggerInt = int2;
+        smallerInt = int1;
+    };
+
+    //While loop to append array
+    arrayOfAll = [];
+    counterInt = smallerInt;
+
+    while (counterInt <= biggerInt) {
+        arrayOfAll.push(counterInt);
+        counterInt += 1;
+    }
+
+    //Call sum function
+    let finalSum = sumArray(arrayOfAll);
+
+    //return
+    return finalSum;
+
 
 };
 
