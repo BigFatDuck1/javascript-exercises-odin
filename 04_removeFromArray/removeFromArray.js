3d8793801e35c1fa0aee976ffe4a8ad0f46ddb00
Clement <hoc1@connect.hku.hk>
2023-09-02 22:10:44 +0800

    04_removeFromArray.js completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+
+    let arrayGiven = args[0];
+    let removeThis = args.slice(1, args.length)
+
+    //let arrayLength = arrayGiven.length;
+
+    for (i = 0; i < args.length - 1; i++) {
+
+        let indexOfTarget = arrayGiven.indexOf(removeThis[i]);
+
+        if (indexOfTarget != -1) {
+            arrayGiven.splice(indexOfTarget, 1)
+        }
+    }
+
+    return arrayGiven;
+
 
 };
 
