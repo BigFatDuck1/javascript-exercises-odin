ad57368ab47f67b4624c7fff21903c0b3b2f94f5
Clement <hoc1@connect.hku.hk>
2023-09-02 23:36:24 +0800

    Commit for 06-leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,30 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    //Check for corrrect input type
+    if (typeof(year) != "number") {
+        return "ERROR";
+    };
+
+    //Divisible by four
+    //Divisible by four hundred
+    //Divisible by one hundred not leap year
+
+    if (year % 4 != 0) {
+        return false;
+    }
+    if (year % 4 == 0) {
+        if (year % 100 != 0) {
+            return true;
+        }
+        else if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    };
 
 };
 
