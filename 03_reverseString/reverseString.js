1a0e403fbf3e5762c116935b3dd83af2d03c572c
Clement <hoc1@connect.hku.hk>
2023-09-02 21:46:58 +0800

    03_reverseString completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,19 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+
+    let stringLength = inputString.length;
+
+    //Convert into array
+    let arrayString = inputString.split(""); //array of SINGLE CHARACTERS
+
+    //Reverse
+    let reversedString = "";
+
+    for (i = 0; i < stringLength; i++) {
+        let poppedCharacter = arrayString.pop();
+        reversedString = reversedString + poppedCharacter;
+    }; 
+
+    return reversedString;
 
 };
 
