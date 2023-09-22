73bccf9b47d55f18b719364664a322d6b1b9fad8
Clement <clementho999@gmail.com>
2023-09-22 13:21:38 +0800

    11 getTheTitles completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,29 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
+
+    let titles_array = [];
+
+    array.forEach(element => {
+        titles_array.push(element.title)
+    });
+
+    return titles_array;
 
 };
+
+//Test
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+]
+
+console.log(getTheTitles(books));
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
