c0b9b80ccbe024f6202b275fa8a244eb3aba598c
Clement <clementho999@gmail.com>
2023-09-22 12:21:30 +0800

    10 Fibonacci completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,31 @@
-const fibonacci = function() {
+const fibonacci = function(nth_number) {
+
+    //Check for negatives
+    if (nth_number <= 0) {
+        return "OOPS";
+    }
+
+    //Generate sequence up to nth_number
+    let fibonacci = [];
+
+    for (i = 0; i < nth_number; i++) {
+        if (i == 0 || i == 1) {
+            fibonacci.push(1)
+            continue
+        }
+        else {
+            let next_number = fibonacci[i - 2] + fibonacci[i - 1];
+            fibonacci.push(next_number);
+        }
+    }
+
+    //Return nth_number
+    console.log(fibonacci);
+    return fibonacci[nth_number - 1];
 
 };
+
+console.log(fibonacci(5));
 
 // Do not edit below this line
 module.exports = fibonacci;
